e8711e1b503082403ef548fe2bbd55a3826818e5
fsociety <fsociety@root>
2021-08-24 15:58:40 +0530

    Server Side Rendering - Next js server side rendering - js fetch - props - props destructing

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import HeroImage from '../components/HeroImage';
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -11,6 +11,27 @@ export default function Home() {
       </Head>
       <Header />
       <HeroImage />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore nearby</h2>
+        </section>
+
+        {/* Server side rendering */}
+        {exploreData?.map((item) => (
+          <h1>{item.location}</h1>
+        ))}
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
