1cbc680d260693a8c345290388bfeaaa15f0eb98
fsociety <fsociety@root>
2021-08-24 16:20:52 +0530

    Small Cards - Explore Nearby section - props through components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import HeroImage from '../components/HeroImage';
+import SmallCard from '../components/SmallCard';
 
 export default function Home({ exploreData }) {
   return (
@@ -12,14 +13,25 @@ export default function Home({ exploreData }) {
       <Header />
       <HeroImage />
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        {/* Small Cards */}
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore nearby</h2>
+          {/* Server side rendering */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ location, distance, img }) => (
+              <SmallCard
+                key={img}
+                location={location}
+                image={img}
+                distance={distance}
+              />
+            ))}
+          </div>
         </section>
 
-        {/* Server side rendering */}
-        {exploreData?.map((item) => (
-          <h1>{item.location}</h1>
-        ))}
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+        </section>
       </main>
     </div>
   );
@@ -29,6 +41,7 @@ export async function getStaticProps() {
   const exploreData = await fetch('https://links.papareact.com/pyp').then(
     (res) => res.json()
   );
+
   return {
     props: {
       exploreData,
