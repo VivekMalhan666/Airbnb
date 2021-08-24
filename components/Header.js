6ee6b0de6fe7b7c9056f4b5c26696e10a5baa63c
fsociety <fsociety@root>
2021-08-24 14:48:30 +0530

    Header User interface - Right side of Header - HeroIcons - Tailwind css

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/solid';
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  SearchIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from '@heroicons/react/solid';
 
 function Header() {
   return (
@@ -13,6 +19,7 @@ function Header() {
           objectPosition="left"
         />
       </div>
+
       {/* Middle - Search */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-md">
         <input
@@ -22,8 +29,16 @@ function Header() {
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
-      {/* Right */}
-      <div></div>
+
+      {/* Right -  */}
+      <div className="flex space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }
