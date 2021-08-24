f1991a20d9db23f07ff3cc7c5cb7ec0e8a0cd1ee
fsociety <fsociety@root>
2021-08-24 17:37:57 +0530

    Calender - React-date-range - Functionality of calender - Theme of calender

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,29 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from '@heroicons/react/solid';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  // To provide a range of selected dates to calender
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
+  // To select dates in calender
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left - Icon */}
@@ -23,6 +44,8 @@ function Header() {
       {/* Middle - Search */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-md">
         <input
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
           type="text"
           placeholder="Start your search"
           className="pl-5 bg-transparent outline-none flex-grow text-gray-600 placeholder-gray-400 text-sm"
@@ -30,7 +53,7 @@ function Header() {
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
-      {/* Right -  */}
+      {/* Right - User Informations*/}
       <div className="flex space-x-4 items-center justify-end text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
@@ -39,6 +62,18 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {/* Calender and Guests */}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 }
